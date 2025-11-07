2b5d85d7151ef8bcb78d1fd26a71fe0d0623a0d8
Marcelo Fornet <mfornet94@gmail.com>
2025-11-07 15:39:24 -0800

    fix cargo installation

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -euo pipefail
 
 # Install cargo
-curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source "$HOME/.cargo/env"
 
 if [ "$(uname -s)" != "Linux" ]; then
     echo "This script is expected to be executed on a linux machine"
